1e7eb3bf4ec36d0b0c65275a3c2f6322d0610d97
Johannes Großmann <grossmann.johannes@t-online.de>
2020-02-16 05:56:42 +0100

    Added sidebar menu.

--- art-gallery/src/views/Home.vue
+++ art-gallery/src/views/Home.vue
@@ -1,18 +1,199 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="menu">
+      <div class="open-action">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="text-box">
+        <h4>2020</h4>
+        <div class="spacer"></div>
+        <h1>Doris Großmann</h1>
+        <div class="spacer"></div>
+        <p>Kunstausstellung Gemälde Gallerie</p>
+      </div>
+      <div class="dash"></div>
+      <div class="menu-art">
+        <div class="h-line"></div>
+        <div id="heart"></div>
+        <div id="diamond"></div>
+        <div id="circle"></div>
+      </div>
+    </div>
+    <div class="content">
+      <div class="section-1">
+        <h1>Doris</h1>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+  import Component from 'vue-class-component'
+  import { Vue } from 'vue-property-decorator'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  @Component
+  export default class Counter extends Vue {
+
   }
-}
 </script>
+
+<style scoped lang="scss">
+  $menu-width: 4.1vw;
+  $border-color: #a3a3a3;
+
+  .wrapper {
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-columns: $menu-width 1fr;
+  }
+
+  .menu {
+    border-right: 1px solid $border-color;
+    position: relative;
+
+    .text-box {
+      position: absolute;
+      top: 70vh;
+      left: 0;
+      transform: rotate(-90deg) translateX(0%);
+      transform-origin: 0 0;
+      white-space: nowrap;
+      height: $menu-width;
+      display: flex;
+      align-items: center;
+
+      .spacer {
+        width: 2.5vw;
+        height: 1px;
+      }
+
+      h1{
+        font-size: 0.8vw;
+        font-weight: 900;
+      }
+
+      h4{
+        font-size: 0.5vw;
+        font-weight: 300;
+      }
+
+      p{
+      font-size: 0.5vw;
+      }
+    }
+
+    .menu-art {
+      box-sizing: content-box;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      padding: 1.6vw 1vw;
+      width: calc(100% - 2 * 1vw);
+      display: flex;
+      flex-direction: column;
+      height: 9vw;
+      align-items: center;
+      justify-content: space-around;
+
+      .h-line {
+        height: 1px;
+        width: 70%;
+        margin-bottom: 3vw;
+        background-color: $border-color;
+      }
+
+      $h-w: 1vw;
+      $h-c: #000;
+
+      #circle {
+        width: 0.9 * $h-w;
+        height: 0.9 * $h-w;
+        background-color: $h-c;
+        border-radius: 50%;
+      }
+
+      #heart {
+        position: relative;
+        width: $h-w;
+        height: 0.9 * $h-w;
+      }
+
+      #heart:before,
+      #heart:after {
+        position: absolute;
+        content: "";
+        left: 0.5 * $h-w;
+        top: 0;
+        width: 0.5 * $h-w;
+        height: 0.8 * $h-w;
+        background: black;
+        border-radius: 0.5 * $h-w 0.5 * $h-w 0 0;
+        transform: rotate(-45deg);
+        transform-origin: 0 100%;
+      }
+
+      #heart:after {
+        left: 0;
+        transform: rotate(45deg);
+        transform-origin: 100% 100%;
+      }
+
+      #diamond {
+        width: 0;
+        height: 0;
+        border: 0.5 * $h-w solid transparent;
+        border-bottom-color: $h-c;
+        position: relative;
+        transform: translateY(-50%);
+      }
+
+      #diamond:after {
+        content: '';
+        position: absolute;
+        left: -0.5 * $h-w;
+        width: 0;
+        height: 0;
+        border: 0.5 * $h-w solid transparent;
+        border-top-color: $h-c;
+        transform: translateY(50%);
+      }
+    }
+  }
+
+  .open-action {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-row-gap: 0.3vw;
+    grid-auto-rows: 0.16vw;
+    padding: 1.6vw 1.2vw;
+    border-bottom: 1px solid $border-color;
+    width: 100%;
+
+    div {
+      background-color: black;
+    }
+  }
+
+  .section-1 {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    h1 {
+      font-size: 80vh;
+      background: url(../stock/picasso-1.jpg) no-repeat center center;
+      background-size: cover;
+      width: 100%;
+      height: 100%;
+      line-height: 100vh;
+      text-align: center;
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
+    }
+  }
+</style>
