32d9f8b630a9281efb22b10c3371d6290bf18674
Johannes Großmann <grossmann.johannes@t-online.de>
2020-02-23 12:41:20 +0100

    Redesigned sidebar.

---
+++ art-gallery/src/components/SideBar.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="menu">
+      <div class="open-action">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div class="text-box">
+        <h4>2020</h4>
+        <div class="spacer"></div>
+        <h1>Doris Großmann</h1>
+        <div class="spacer"></div>
+        <p>Kunstausstellung Gemälde Gallerie</p>
+      </div>
+      <div class="dash"></div>
+      <div class="menu-art">
+        <div class="h-line"></div>
+        <div id="heart"></div>
+        <div id="diamond"></div>
+        <div id="circle"></div>
+      </div>
+    </div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+export default class SideBar extends Vue {
+
+}
+</script>
+
+<style scoped lang="scss">
+  @import "../scss/constants";
+
+  .menu {
+    border-right: 1px solid $border-color;
+    position: fixed;
+    left: 0;
+    top: 0;
+    height: 100vh;
+    width: $menu-width;
+
+    .text-box {
+      position: absolute;
+      top: 75vh;
+      left: 0;
+      transform: rotate(-90deg) translateX(0%);
+      transform-origin: 0 0;
+      white-space: nowrap;
+      height: $menu-width;
+      display: flex;
+      align-items: center;
+
+      .spacer {
+        width: 2.5vw;
+        height: 1px;
+      }
+
+      h1 {
+        font-size: 1.4vw;
+        font-weight: 900;
+      }
+
+      $sub-font: 0.7vw;
+
+      h4 {
+        font-size: $sub-font;
+        font-weight: 300;
+      }
+
+      p {
+        font-size: $sub-font;
+      }
+    }
+
+    .menu-art {
+      box-sizing: content-box;
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      padding: 1.6vw 1vw;
+      width: calc(100% - 2 * 1vw);
+      display: flex;
+      flex-direction: column;
+      height: 9vw;
+      align-items: center;
+      justify-content: space-around;
+
+      .h-line {
+        height: 1px;
+        width: 70%;
+        margin-bottom: 3vw;
+        background-color: $border-color;
+      }
+
+      $h-w: 1vw;
+      $h-c: #000;
+
+      #circle {
+        width: 0.9 * $h-w;
+        height: 0.9 * $h-w;
+        background-color: $h-c;
+        border-radius: 50%;
+      }
+
+      #heart {
+        position: relative;
+        width: $h-w;
+        height: 0.9 * $h-w;
+      }
+
+      #heart:before,
+      #heart:after {
+        position: absolute;
+        content: "";
+        left: 0.5 * $h-w;
+        top: 0;
+        width: 0.5 * $h-w;
+        height: 0.8 * $h-w;
+        background: black;
+        border-radius: 0.5 * $h-w 0.5 * $h-w 0 0;
+        transform: rotate(-45deg);
+        transform-origin: 0 100%;
+      }
+
+      #heart:after {
+        left: 0;
+        transform: rotate(45deg);
+        transform-origin: 100% 100%;
+      }
+
+      #diamond {
+        width: 0;
+        height: 0;
+        border: 0.5 * $h-w solid transparent;
+        border-bottom-color: $h-c;
+        position: relative;
+        transform: translateY(-50%);
+      }
+
+      #diamond:after {
+        content: '';
+        position: absolute;
+        left: -0.5 * $h-w;
+        width: 0;
+        height: 0;
+        border: 0.5 * $h-w solid transparent;
+        border-top-color: $h-c;
+        transform: translateY(50%);
+      }
+    }
+  }
+
+  .open-action {
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-row-gap: 0.3vw;
+    grid-auto-rows: 0.2vw;
+    padding: 1.6vw 1.6vw;
+    border-bottom: 1px solid $border-color;
+    width: 100%;
+
+    div {
+      background-color: black;
+    }
+  }
+</style>
--- art-gallery/src/views/Home.vue
+++ art-gallery/src/views/Home.vue
@@ -1,26 +1,6 @@
 <template>
   <div class="wrapper">
-    <div class="menu">
-      <div class="open-action">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-      <div class="text-box">
-        <h4>2020</h4>
-        <div class="spacer"></div>
-        <h1>Doris Großmann</h1>
-        <div class="spacer"></div>
-        <p>Kunstausstellung Gemälde Gallerie</p>
-      </div>
-      <div class="dash"></div>
-      <div class="menu-art">
-        <div class="h-line"></div>
-        <div id="heart"></div>
-        <div id="diamond"></div>
-        <div id="circle"></div>
-      </div>
-    </div>
+    <side-bar></side-bar>
     <div class="content">
       <div class="section-1">
         <div class="img-box"><div class="content" v-bind:style="{backgroundImage: `url(${images[12].path})`}"></div></div>
@@ -70,16 +50,17 @@
   import Component from 'vue-class-component';
   import { Vue } from 'vue-property-decorator';
   import { IMAGES } from '@/assets/content';
-
-  @Component
+  import SideBar from '@/components/SideBar.vue';
+  @Component({
+    components: { SideBar }
+  })
   export default class Counter extends Vue {
     images = IMAGES;
   }
 </script>
 
 <style scoped lang="scss">
-  $menu-width: 4.1vw;
-  $border-color: #a3a3a3;
+  @import "../scss/constants";
 
   .wrapper {
     width: 100%;
@@ -88,137 +69,6 @@
 
     .content {
       width: 100%;
-    }
-  }
-
-  .menu {
-    border-right: 1px solid $border-color;
-    position: fixed;
-    left: 0;
-    top: 0;
-    height: 100vh;
-    width: $menu-width;
-
-    .text-box {
-      position: absolute;
-      top: 70vh;
-      left: 0;
-      transform: rotate(-90deg) translateX(0%);
-      transform-origin: 0 0;
-      white-space: nowrap;
-      height: $menu-width;
-      display: flex;
-      align-items: center;
-
-      .spacer {
-        width: 2.5vw;
-        height: 1px;
-      }
-
-      h1 {
-        font-size: 0.8vw;
-        font-weight: 900;
-      }
-
-      h4 {
-        font-size: 0.5vw;
-        font-weight: 300;
-      }
-
-      p {
-        font-size: 0.5vw;
-      }
-    }
-
-    .menu-art {
-      box-sizing: content-box;
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      padding: 1.6vw 1vw;
-      width: calc(100% - 2 * 1vw);
-      display: flex;
-      flex-direction: column;
-      height: 9vw;
-      align-items: center;
-      justify-content: space-around;
-
-      .h-line {
-        height: 1px;
-        width: 70%;
-        margin-bottom: 3vw;
-        background-color: $border-color;
-      }
-
-      $h-w: 1vw;
-      $h-c: #000;
-
-      #circle {
-        width: 0.9 * $h-w;
-        height: 0.9 * $h-w;
-        background-color: $h-c;
-        border-radius: 50%;
-      }
-
-      #heart {
-        position: relative;
-        width: $h-w;
-        height: 0.9 * $h-w;
-      }
-
-      #heart:before,
-      #heart:after {
-        position: absolute;
-        content: "";
-        left: 0.5 * $h-w;
-        top: 0;
-        width: 0.5 * $h-w;
-        height: 0.8 * $h-w;
-        background: black;
-        border-radius: 0.5 * $h-w 0.5 * $h-w 0 0;
-        transform: rotate(-45deg);
-        transform-origin: 0 100%;
-      }
-
-      #heart:after {
-        left: 0;
-        transform: rotate(45deg);
-        transform-origin: 100% 100%;
-      }
-
-      #diamond {
-        width: 0;
-        height: 0;
-        border: 0.5 * $h-w solid transparent;
-        border-bottom-color: $h-c;
-        position: relative;
-        transform: translateY(-50%);
-      }
-
-      #diamond:after {
-        content: '';
-        position: absolute;
-        left: -0.5 * $h-w;
-        width: 0;
-        height: 0;
-        border: 0.5 * $h-w solid transparent;
-        border-top-color: $h-c;
-        transform: translateY(50%);
-      }
-    }
-  }
-
-  .open-action {
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-row-gap: 0.3vw;
-    grid-auto-rows: 0.16vw;
-    padding: 1.6vw 1.2vw;
-    border-bottom: 1px solid $border-color;
-    width: 100%;
-
-    div {
-      background-color: black;
     }
   }
 
@@ -250,6 +100,9 @@
         right: 0;
         background-size: cover;
         background-position: center;
+        &:not(.text){
+          filter: contrast(0.8) brightness(1.2);
+        }
 
         &.text {
           display: flex;
